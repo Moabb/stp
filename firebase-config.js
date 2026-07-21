ce8c20daa4dcd37724b291ab2b6be1602465855d
Moabb <moabbcel@gmail.com>
2026-07-21 11:38:21 -0300

    correção para config.html

--- firebase-config.js
+++ firebase-config.js
@@ -1,55 +1,50 @@
-// firebase-config.js - Firebase v10+ (ES Modules)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { 
   getAuth, 
   signInWithEmailAndPassword, 
   createUserWithEmailAndPassword, 
-  signOut, 
-  onAuthStateChanged 
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+  onAuthStateChanged, 
+  signOut 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { 
   getFirestore, 
   collection, 
   doc, 
   setDoc, 
   addDoc, 
-  updateDoc, 
-  deleteDoc, 
-  onSnapshot, 
+  getDocs, 
   query, 
-  where, 
-  orderBy 
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+  where 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// COLE AQUI AS SUAS CHAVES DO FIREBASE CONSOLE:
+// Sua configuração do Firebase SDK
 const firebaseConfig = {
-  apiKey: "AIzaSyDVZSlNvtLOER3YdotvGi-G7VvDtSQwV7M",
-  authDomain: "sistema-team-penning.firebaseapp.com",
-  projectId: "sistema-team-penning",
-  storageBucket: "sistema-team-penning.firebasestorage.app",
-  messagingSenderId: "1025888364244",
-  appId: "1:1025888364244:web:d4c5d0582899a855ddbd41"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJECT_ID.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_PROJECT_ID.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
 };
 
-// Inicialização
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Exportando métodos utilitários
+// Exporta as instâncias e todas as funções utilizadas nos scripts
 export { 
-  signInWithEmailAndPassword, 
-  createUserWithEmailAndPassword, 
-  signOut, 
-  onAuthStateChanged,
-  collection, 
+  auth, 
+  db, 
   doc, 
   setDoc, 
   addDoc, 
-  updateDoc, 
-  deleteDoc, 
-  onSnapshot, 
+  getDocs, 
+  collection, 
   query, 
   where, 
-  orderBy 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword, 
+  onAuthStateChanged, 
+  signOut 
 };
